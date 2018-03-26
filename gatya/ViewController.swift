db1465fc35c4ab4a136ac08a55d919de151c5d2e
田嶋智洋 <kapiha7@gmail.com>
2018-03-26 18:01:34 +0900

    Initial Commit

---
+++ gatya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gatya
+//
+//  Created by 田嶋智洋 on 2018/03/26.
+//  Copyright © 2018年 田嶋智洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
